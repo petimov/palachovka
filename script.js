ef599f554ef98dca5e3ad199cb57db880b33c7e5
John Doe <john@example.com>
2025-08-05 22:14:36 +0200

    added another section

--- script.js
+++ script.js
@@ -4,21 +4,19 @@ window.onload = function () {
   document.body.addEventListener("mousemove", function (e) {
     cursor.style.left = e.clientX + "px";
     cursor.style.top = e.clientY + "px";
-    cursor.style.visibility = "visible";
   });
 
   const links = document.querySelectorAll("a");
 
   links.forEach(link => {
   link.addEventListener("mouseenter", () => {
-    cursor.style.transform = "scale(1.8)";
+    cursor.style.scale = "1.8";
     cursor.style.opacity = "0.2";
   });
 
   link.addEventListener("mouseleave", () => {
-    cursor.style.transform = "scale(1)";
+    cursor.style.scale = "1";
     cursor.style.opacity = "1";
-    cursor.style.transform = "translateX(-50%) translateY(-50%)";
   });
 });
 
